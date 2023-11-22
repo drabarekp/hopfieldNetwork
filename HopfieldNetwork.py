f1aaac19d43599391b7b6534db91e1c6cd74d29e
szgorski <101061451+szgorski@users.noreply.github.com>
2023-11-22 08:09:37 +0100

    tests finished

--- HopfieldNetwork.py
+++ HopfieldNetwork.py
@@ -1,7 +1,5 @@
 import copy
 
-import numpy as np
-
 from dataManager import *
 
 
@@ -14,10 +12,7 @@ class HopfieldNetwork:
         self.neurons = np.zeros(self.length, dtype='float16')
         self.weights = np.zeros((self.length, self.length), dtype='float16')
 
-        self.energies = []
-        self.calc_energy()
-        self.scores = []
-        self.calc_score()
+        self.energies = [self.calc_energy()]
         self.states = [copy.deepcopy(self.neurons)]
 
         if learning == HEBB:
@@ -27,27 +22,22 @@ class HopfieldNetwork:
         else:
             raise ValueError("Pick Hebb or Oja!")
 
+    def reset_neurons(self):
+        self.neurons = np.zeros(self.length, dtype='float16')
+        self.energies = [np.inf]
+        self.states = [copy.deepcopy(self.neurons)]
+
     # no parameters reset implemented - use this function one time only!
     def load_patterns_and_learn(self, patterns, eta=0.001, iterations=100):
         self.patterns = np.array(patterns)
         self.learning_func(self.patterns, eta, iterations)
 
-    def hebb(self, patterns, eta=None, iterations=None):  # , collect=True, plot=True):
-        # patterns = np.array(patterns)
+    def hebb(self, patterns, eta=None, iterations=None):
         self.weights = (patterns.T @ patterns) / len(patterns)  # self.length
-        # print('finished')
         for i in range(self.length):
             self.weights[i][i] = 0.0
 
-        # if collect:
-        #     self.learning.append(copy.deepcopy(self.weights))
-        # if plot:
-        #     self.draw_learning()
-
-    def oja(self, patterns, eta, iterations):  # , collect=True, plot=True):
-        # self.hebb(patterns)  # , collect=True, plot=False)
-        # self.weights = np.zeros((self.length, self.length))
-        # new_weights = np.zeros((self.length, self.length))
+    def oja(self, patterns, eta, iterations):
 
         self.weights = np.float16(np.random.uniform(-1, 1, size=(self.length, self.length)))
         for it in range(iterations):
@@ -57,23 +47,11 @@ class HopfieldNetwork:
                 for j in range(self.length):
                     self.weights[i][j] = (1 - eta) * self.weights[i][j] + eta * np.sum(
                         patterns[:, j] * (patterns[:, i] - patterns[:, j] * self.weights[i][j])) / len(patterns)
-                # print(i)
                 self.weights[i][i] = 0.0
 
             n_sc = np.linalg.norm(old_weights - self.weights)
-            # print(n_sc)
             if n_sc < 1E-10:
                 break
-        # V = np.sum(self.weights * self.neurons)
-        #
-        # for i in range(iterations):
-        #     neurons_matrix = np.array([np.full(self.length, self.neurons[i]) for i in range(self.length)])
-        #     self.weights = self.weights + eta * V * (neurons_matrix - V * self.weights)
-
-        # if collect:
-        #     self.learning.append(copy.deepcopy(self.weights))
-        # if plot:
-        #     self.draw_learning()
 
     def update_one(self):
         index = np.random.randint(0, self.length)
@@ -84,27 +62,19 @@ class HopfieldNetwork:
         else:
             self.neurons[index] = -1
 
-    def update_all(self):
-        new_neurons = np.zeros(self.length, dtype='float16')
-        for index in range(self.length):
-            activation = np.sum(self.weights[index, :] * self.neurons)
-            if activation > 0:
-                new_neurons[index] = 1
-            else:
-                new_neurons[index] = -1
-        self.neurons = new_neurons
-
     def update_sync(self):
         new_neurons = self.neurons.copy()
         for i in range(self.length):
             activation = np.dot(self.weights[i, :], self.neurons)
+
             if activation > 0:
                 new_neurons[i] = 1
             else:
                 new_neurons[i] = -1
+
         self.neurons = new_neurons
-        # self.states.append(copy.deepcopy(self.neurons))
-        # plot_dataset(self.states, self.size, enum=True)
+        self.states.append(copy.deepcopy(self.neurons))
+        self.energies.append(self.calc_energy())
 
     # updates fraction of neurons given by fraction_updated
     def update_async(self, fraction_updated):
@@ -113,32 +83,61 @@ class HopfieldNetwork:
                 self.update_one()
 
             self.states.append(copy.deepcopy(self.neurons))
-        plot_dataset(self.states, self.size, enum=True)
-
-        # numbers = self.length * fraction_updated
-        # for _ in range(int(numbers)):
-        #     self.update_one()
+            self.energies.append(self.calc_energy())
 
     def calc_energy(self):
-        self.energies.append(-np.dot(np.dot(self.neurons.T, self.weights), self.neurons) / 2)
+        energy = 0
+        # loop overcomes float16 precision and memory problems
+        for i in range(self.length):
+            energy += np.sum(np.float64(self.weights[i]) * np.float64(self.neurons) * np.float64(self.neurons[i]))
+        return energy / 2
 
-    def calc_score(self):
+    def calc_score(self, max_iter, error, rands):
         visualisations = []
-        neurons_copy = copy.deepcopy(self.neurons)
         score = []
 
-        for pattern in self.patterns:
-            self.neurons = copy.deepcopy(pattern)
-            self.update_all()
+        if rands > 0:
+            for _ in range(rands):
+                self.reset_neurons()
+                self.neurons = np.float16(np.random.choice([1, -1], size=self.length))
+                self.energies = [self.calc_energy()]
+                self.states = [copy.deepcopy(self.neurons)]
 
-            score.append(np.mean(pattern == self.neurons))
-            visualisations.append(self.neurons)
+                self.update_sync()
+                for i in range(max_iter - 1):
+                    if (np.array_equal(self.states[i + 1], self.states[i])
+                            and self.energies[i] - self.energies[i + 1] < 1E-2):
+                        break
+                    self.update_sync()
 
-        self.neurons = neurons_copy
+                score.append(np.max([np.mean(self.patterns[i] == self.neurons) for i in range(len(self.patterns))]))
+                visualisations.append(self.states)
+        else:
+            for pattern in self.patterns:
+                self.reset_neurons()
+                if error > 0:
+                    self.neurons = damage_pattern(pattern, error)
+                else:
+                    self.neurons = copy.deepcopy(pattern)
+                self.energies = [self.calc_energy()]
+                self.states = [copy.deepcopy(self.neurons)]
+
+                self.update_sync()
+                for i in range(max_iter - 1):
+                    # network cant 'over-train'
+                    if (np.array_equal(self.states[i + 1], self.states[i])
+                            and self.energies[i] - self.energies[i + 1] < 1E-2):
+                        break
+                    self.update_sync()
+
+                score.append(np.mean(pattern == self.neurons))
+                visualisations.append(self.states)
+
+        self.reset_neurons()
         return score, visualisations
 
-    def report_score(self):
-        score, visualisations = self.calc_score()
+    def report_score(self, max_iter=100, error=0, rands=0):
+        score, visualisations = self.calc_score(max_iter, error, rands)
         stabilities = np.count_nonzero(np.abs(np.subtract(score, 1)) < 1E-8)
         avg_score = np.mean(score)
 
@@ -153,10 +152,3 @@ class HopfieldNetwork:
     def draw_weights(self):
         plt.figure(figsize=[20, 20])
         plt.imshow(np.array(self.weights).reshape(self.length, self.length), cmap='viridis')
-
-    # def draw_learning(self):
-    #     plt.figure(figsize=[20, 5 * int(np.ceil(len(self.learning) / 4))])
-    #     for it in range(len(self.learning)):
-    #         plt.subplot(int(np.ceil(len(self.learning) / 4)), 4, it + 1)
-    #         plt.title(str(it))
-    #         plt.imshow(np.array(self.learning[it] * -1).reshape((self.length, self.length)), cmap='viridis')
